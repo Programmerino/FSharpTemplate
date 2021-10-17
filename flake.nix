2727dd338890c458449a864e368513e99ed8cce9
Davis Davalos-DeLosh <github@tripd.anonaddy.com>
2021-10-16 20:16:57 -0600

    fixing workflow and versioning

--- flake.nix
+++ flake.nix
@@ -1,11 +1,14 @@
 {
   description = "FSharpTemplate";
 
+  nixConfig.bash-prompt = "\[nix-develop\]$ ";
+
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.dotnet.url = "github:Programmerino/dotnet-nix";
+  inputs.dotnet.url = "/home/davis/Documents/Personal/CSProjects/dotnet-nix";
+  #inputs.dotnet.url = "github:Programmerino/dotnet-nix";
   outputs = { self, nixpkgs, flake-utils, dotnet }:
     flake-utils.lib.eachSystem(["x86_64-linux" "aarch64-linux"]) (system:
       let
@@ -13,11 +16,13 @@
           inherit system;
         };
         name = "FSharpTemplate";
-        version = "0.0.0";
+        version = let _ver = builtins.getEnv "GITVERSION_NUGETVERSIONV2"; in if _ver == "" then "0.0.0" else "_ver";
         sdk = pkgs.dotnetCorePackages.sdk_5_0;
 
       in rec {
           devShell = pkgs.mkShell {
+            inherit name;
+            inherit version;
             DOTNET_CLI_HOME = "/tmp/dotnet_cli";
             buildInputs = defaultPackage.nativeBuildInputs ++ [sdk];
             DOTNET_ROOT = "${sdk}";
