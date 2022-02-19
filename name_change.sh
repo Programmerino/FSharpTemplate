495aec02ea37277167c7b3da4e0c04bb0eba4aac
Davis Davalos-DeLosh <davis@0bit.dev>
2022-02-19 16:11:50 -0700

    Changing project creation pipeline

--- name_change.sh
+++ name_change.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-rm -rf .git
 find . -type f -exec sed -i "s/FSharpTemplate/$1/g" {} +
 find . -type f -exec bash -c "mv \$0 \${0/FSharpTemplate/$1}" {} \;
 rm name_change.sh
+git remote remove origin
+git add .
+git commit -m "rename project to $1"
